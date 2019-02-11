8eda00c546570729c62c602cb138dea061b67b83
RMH <rory.hennessey@gmail.com>
2019-02-11 16:58:04 -0500

    header and cards are created. need to format and create master

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+    return <div className = "card-text"> <p className="card-intro">Get started with React</p> 
+        <p className="card-copy">React makes it painless to create interactive UIs. Design simple views for each state in your application.</p> 
+        <p className="card-link">reactjs.org</p>
+        </div>;
+}
+
+
+export default CardContent;
